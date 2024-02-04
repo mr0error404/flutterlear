e71d5b0d95cf4499ec16d1d64dee2351ba4f76d8
macbookpro <Eng.abdullahyseein@gmail.com>
2024-02-04 18:59:35 +0300

    e

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,7 +5,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold(  //erf
       appBar: AppBar( 
         backgroundColor: Colors.red,
         leading: const Icon(
